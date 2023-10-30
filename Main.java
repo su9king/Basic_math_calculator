fb529abd4ac12f4a7c00cf196d633a738b4ff0dc
su9king <su99king@gmail.com>
2023-10-30 20:28:39 +0900

    some updating

--- Main.java
+++ Main.java
@@ -1,22 +1,23 @@
 package Basic_math_calculator;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Main{
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        //setting_option Screen = new setting_option();
 
-        creating_font message = new creating_font("하이루!",100,50,500,300);
-        message.data_check();
         
          // JFrame 생성
         JFrame frame = new JFrame("Text Output Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         // 출력할 문자열
-        String text = "Hello, World!";
+        String text = "HHHHHH";
         
         // JLabel 생성 및 문자열 설정
         JLabel label = new JLabel(text);
@@ -27,14 +28,18 @@ public class Main{
         // JFrame 크기 및 위치 설정
         frame.setSize(300, 200);
         frame.setLocationRelativeTo(null);  // 화면 중앙에 표시
+
+        // JFrame 
+        frame.setLayout(new FlowLayout());
         
         // JFrame을 보이도록 설정
+        frame.add(new JButton("뭘봐 버튼이다임마"));
         frame.setVisible(true);
+
         
+
     }
-
 }
-
 
 
 //2023.10.26 목 23:21
