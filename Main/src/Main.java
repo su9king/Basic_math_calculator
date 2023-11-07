88ad3baf38835803b3c7e758f092b37fb7847e14
su9king <su99king@gmail.com>
2023-11-07 19:54:15 +0900

    daily coading

---
+++ Main/src/Main.java
@@ -0,0 +1,72 @@
+
+
+import javax.swing.*;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        // JFrame 생성
+        JFrame frame = new JFrame("Text Output Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        // JFrame 크기 및 위치 설정
+        frame.setSize(300, 200);
+        frame.setLocationRelativeTo(null);  // 화면 중앙에 표시
+
+        // JFrame
+        frame.setLayout(new FlowLayout());
+
+        // JFrame을 보이도록 설정
+        JButton plus_button = new JButton("덧셈");
+
+        ActionListener listenera = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JLabel label = new JLabel("첫번째 숫자를 입력하세요.");
+                JTextField txt = new JTextField(10);
+
+                JButton complete_button = new JButton("입력완료");
+                frame.add(complete_button);
+                frame.add(label);
+                frame.add(txt);
+
+                frame.setVisible(true);
+
+                ActionListener listenerb = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+
+                        frame.remove(label);
+                        JLabel label = new JLabel("입력한 값은 : "+ txt.getText());
+
+                        frame.add(label);
+                        frame.setVisible(true);
+
+
+                    }
+                };
+                complete_button.addActionListener(listenerb);
+            }
+        };
+
+        plus_button.addActionListener(listenera);
+
+
+        frame.add(plus_button);
+
+        frame.setVisible(true);
+
+
+
+    }
+
+}
